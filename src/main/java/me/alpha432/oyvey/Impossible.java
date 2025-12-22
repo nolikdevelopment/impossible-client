321b46a3c9895e268d7c2721c0df52193de69c88
nolikdevelopment <knik7536@yandex.ru>
2025-12-22 20:39:16 +0300

    апд

--- src/main/java/me/alpha432/oyvey/Impossible.java
+++ src/main/java/me/alpha432/oyvey/Impossible.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Impossible implements ModInitializer, ClientModInitializer {
-    public static final String NAME = "Impossible Client v2 - 1.21.10 - 21/12/2025 14:27";
+    public static final String NAME = "Impossible Client v2 - 1.21.10 - 22/12/2025 20:39";
     public static float TIMER = 1f;
 
     public static final Logger LOGGER = LogManager.getLogger("");
